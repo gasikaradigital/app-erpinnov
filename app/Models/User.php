2c1bedf6832f7dccf93434255a1966141ba69b7e
bezara florent <bezaraflorent@gmail.com>
2024-09-24 08:31:10 +0300

    Résolution du conflit dans User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Models\Role;
 use Laravel\Jetstream\HasProfilePhoto;
@@ -22,27 +21,20 @@ class User extends Authenticatable
     use TwoFactorAuthenticatable;
     use HasRoles;
     use SoftDeletes;
-    protected $dates = ['deleted_at'];
-<<<<<<< HEAD
 
-=======
+    protected $dates = ['deleted_at'];
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
->>>>>>> 4888495f40227bc0ff0c15f5a63645a0928b448c
     protected $fillable = [
         'name',
         'email',
         'password',
-<<<<<<< HEAD
         'email_verified_at',
         'is_active',
-    ];
-
-=======
-        'email_verified_at'
     ];
 
     /**
@@ -50,7 +42,6 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
->>>>>>> 4888495f40227bc0ff0c15f5a63645a0928b448c
     protected $hidden = [
         'password',
         'remember_token',
@@ -58,32 +49,25 @@ class User extends Authenticatable
         'two_factor_secret',
     ];
 
-<<<<<<< HEAD
-=======
     /**
      * The accessors to append to the model's array form.
      *
      * @var array<int, string>
      */
->>>>>>> 4888495f40227bc0ff0c15f5a63645a0928b448c
     protected $appends = [
         'profile_photo_url',
     ];
 
-<<<<<<< HEAD
-=======
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
->>>>>>> 4888495f40227bc0ff0c15f5a63645a0928b448c
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-<<<<<<< HEAD
             'is_active' => 'boolean'
         ];
     }
@@ -109,19 +93,11 @@ class User extends Authenticatable
         return $this->hasMany(Instance::class);
     }
 
-=======
-        ];
-    }
-
-
-    // Relation avec le modèle Subscription (Un utilisateur peut avoir plusieurs abonnements)
->>>>>>> 4888495f40227bc0ff0c15f5a63645a0928b448c
     public function subscriptions()
     {
         return $this->hasMany(Subscription::class);
     }
 
-<<<<<<< HEAD
     public function activeSubscription()
     {
         return $this->subscriptions()->where('status', 'active')->first();
@@ -152,12 +128,5 @@ class User extends Authenticatable
             return 'Illimité';
         }
         return max(0, $activePlan->instance_limit - $this->instances()->count());
-=======
-    // Relation avec le modèle Instance (Un utilisateur peut créer plusieurs instances Dolibarr)
-    public function instances()
-    {
-        return $this->hasMany(Instance::class);
->>>>>>> 4888495f40227bc0ff0c15f5a63645a0928b448c
     }
-
 }
